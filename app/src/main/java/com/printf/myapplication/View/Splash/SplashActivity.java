643e5a3c185fb6a8dbb8ae9cdd3359e963d8db07
AndroidJester <duah14@outlook.com>
2021-07-07 13:49:38 +0000

    Base Update

--- app/src/main/java/com/printf/myapplication/View/Splash/SplashActivity.java
+++ app/src/main/java/com/printf/myapplication/View/Splash/SplashActivity.java
@@ -1,14 +1,14 @@
-package com.printf.myapplication;
+package com.printf.myapplication.View.Splash;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
     }
 }
